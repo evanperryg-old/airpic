822ad93db1d43a63535a3a3b4d04c90e5d580a05
markushunter <33558720+markushunter@users.noreply.github.com>
2017-12-20 10:49:12 -0600

    The main now controlles the status LED using a Gyro

--- main.c
+++ main.c
@@ -30,19 +30,41 @@
 #include "airpic-libs/i2c-peripherals/airpic-gyro.h"
 #include "airpic-libs/airpic-serialgps.h"
 
+
+//Stuff I added ================================================================
+
+int currentColor;
+int xValue;
+
+
 void airpic_timer_isr(void)
 {
     if(i2c_error_buscollision)
-        statusLED_setStatus(STATUSLED_FASTBLINK | STATUSLED_ORANGE);
+    {
+       statusLED_setStatus(STATUSLED_FASTBLINK | STATUSLED_ORANGE);
+       currentColor = STATUSLED_ORANGE;
+    }
     
     else if(serial_rcvbuffer_error_overflow)
+    {
         statusLED_setStatus(STATUSLED_FASTBLINK | STATUSLED_RED);
+        currentColor = STATUSLED_RED;
+    }
     
-    else if(serial_receiver_active)
-        statusLED_setStatus(STATUSLED_SOLID | STATUSLED_MAGENTA);
+//    else if(serial_receiver_active)
+//    {
+//       statusLED_setStatus(STATUSLED_SOLID | STATUSLED_MAGENTA);
+//       allGood = 0;
+//       currentColor = STATUSLED_MAGENTA;
+//    }
     
-    else
-        statusLED_setStatus(STATUSLED_LONGBLINK | STATUSLED_BLUE);
+    
+ //   else
+ //       allGood = 1;
+     //   statusLED_setStatus(STATUSLED_LONGBLINK | STATUSLED_BLUE);
+    
+   
+ 
     
     airpic_timer_isr_exit;              //THIS MUST BE AT THE END OF THE ISR IN ORDER FOR IT TO WORK!
 }
@@ -53,13 +75,13 @@ int main(void)
     // It sets the clock cycle to 16MHz, disables the analog-to-digital converters
     // on the I/O pins, and makes sure pins are correctly configured as inputs/outputs.
     airpic_config();                
-    
+   
     // Set up the airpic-timer library's features. The macros used will enable the 
     // interrupt shown at the top of the code, and tell that interrupt to trigger every 
     // 100 milliseconds. Using the 'AIRPIC_TIMER_DEFAULT' macro will do the exact same 
     // thing, but doing it this way shows how to use bitwise OR operators to append 
     // the macros together.
-    timer_config(AIRPIC_TIMER_INTERRUPT_ENABLE | AIRPIC_TIMER_PERIOD_100MS);
+    timer_config(AIRPIC_TIMER_INTERRUPT_ENABLE | AIRPIC_TIMER_PERIOD_20MS);
     
     // Set up the airpic-i2c library's features. You can select a 400kHz baudrate using 
     // the 'AIRPIC_I2C_BAUDRATE_100K' macro, or you can select a 100kHz baudrate using
@@ -76,22 +98,49 @@ int main(void)
     // the gyro will behave the way we want it to. If the gyro is not connected, trying to 
     // initialize it may result in the microcontroller hanging in an infinite while() loop,
     // so don't initialize Gyros that aren't actually rhere.
-    //gyro1_init();
+    gyro1_init();
+    //gyro1_calibrate();
     
     // Send out configuration messages to Gyro 2. Cannot be done before running i2c_config().
     // This should always be done in the initialization section of the code, to ensure
     // the gyro will behave the way we want it to. If the gyro is not connected, trying to 
     // initialize it may result in the microcontroller hanging in an infinite while() loop,
     // so don't initialize Gyros that aren't actually rhere.
-    //gyro2_init();
+    gyro2_init();
     
     // Configure UART1 to receive TTL-formatted serial data from the GPS.
     serialGPS_config();
     
+    
+    motorController_init(50.0);
+    
+    
+
+    
+    
+    
+    
     while(1)
     {
+        motor_write(15, 300); //500 is max, 250 is min
         
+        gyro1_refresh();
+        xValue = gyro1_getX();
+        
+        
+        if(xValue < 0)
+        {
+            if(currentColor != STATUSLED_GREEN)
+                statusLED_setStatus(STATUSLED_SOLID | STATUSLED_GREEN);
+        }
+        else
+        {
+            if(currentColor != STATUSLED_TEAL)
+                statusLED_setStatus(STATUSLED_SOLID | STATUSLED_TEAL);
+        }
+
     }
     
     return 0;
 }
+
